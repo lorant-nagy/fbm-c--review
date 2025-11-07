f72c49edfc950bfe7fe2382c27460104580664d6
Lorant Nagy <lorantnagy28@gmail.com>
2025-11-07 17:26:12 +0100

    python

---
+++ python/read_data.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+BASE_DIR = "/Users/lorantnagy/Documents/CODE/MLDTRADE/fbm-c--review/data"
+
+def read_data(BASE_DIR = BASE_DIR):
+    file_count = 0
+    for file in os.listdir(BASE_DIR):
+        if file.endswith('.csv'):
+            file_count += 1
+
+    data = pd.DataFrame()
+    for i in range(1, file_count + 1):
+        df = pd.read_csv(f'../../data/fbm_{i}.csv', sep=',')
+        if 'time' not in data.columns:
+            data['time'] = df['t']
+        data[f'path_{i}'] = df['BH']
+
+    return data
--- src/main.c
+++ src/main.c
@@ -8,33 +8,40 @@ int main(void) {
     
     srand((unsigned)time(NULL));
 
+    char filename[50];
+
+    int target_path_count = 5;
+
     double H = 0.7;
     int    n = 128;
     double T = 128.0;
+    int counter = 1;
     double *path = NULL;
     double *times = malloc((n + 1) * sizeof *times);
-
-    path = simulate_fBm(H, n, T);
 
     for (int i = 0; i <= n; ++i) {
         double t = T * (double)i / (double)n;
         times[i] = (double)(t);
     }
 
-    // print times array elements
-    for (int i = 0; i <= n; ++i) {
-        printf("times[%d] = %.17g\n", i, times[i]);
-    }
+    while (counter <= target_path_count) {
+        
+        path = simulate_fBm(H, n, T);
+        // Save full path to CSV (columns: t, B_H(t))
+        // create a variable holding the file name
+        sprintf(filename, "data/fbm_%d.csv", counter);
+        FILE *fp = fopen(filename, "w");
+        if (!fp) { perror("fopen"); free(path); free(times); return 1; }
+        fprintf(fp, "t,BH\n");
+        for (int i = 0; i <= n; ++i) {
+            double t = T * (double)i / (double)n;
+            fprintf(fp, "%.17g,%.17g\n", t, path[i]);
+        }
+        fclose(fp);
 
-    // Save full path to CSV (columns: t, B_H(t))
-    FILE *fp = fopen("fbm.csv", "w");
-    if (!fp) { perror("fopen"); free(path); return 1; }
-    fprintf(fp, "t,BH\n");
-    for (int i = 0; i <= n; ++i) {
-        double t = T * (double)i / (double)n;
-        fprintf(fp, "%.17g,%.17g\n", t, path[i]);
+        counter++;
+
     }
-    fclose(fp);
 
     free(path);
     free(times);
